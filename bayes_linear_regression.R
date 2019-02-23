867c7870c292cc39923b22c615a416414df22ae4
Kristyn Pantoja <kristynp@tamu.edu>
2019-02-23 13:46:20 -0600

    created linear regression w prior on beta, but resulting samples are not as regular as in case where beta is known/assumed...

--- bayes_linear_regression.R
+++ bayes_linear_regression.R
@@ -8,31 +8,47 @@
 #  epsilon_i ~ N(0, sigma_e^2)
 #  y_i ~ N(x_i beta, sigma_e^2), beta = {beta0, beta1}
 
+# but now we put a prior on beta:
+# beta_i ~ N(mean_beta_i, var_mean)
 
+# and now we want the distances between the means' posterior distributions
+# beta_i | y, x ~ N(m, s)
+# where m = ((1 / sigma_e^2) * y + (1 / sigma_mean^2) * mean_beta_i) / ((1 / sigma_e^2) + (1 / sigma_mean^2))
+# and s = ((1 / sigma_e^2) + (1 / sigma_mean^2))^(-1)
 
+# note that this can be extended to the case of multiple observed y at a given x, in which case:
+# beta_i | y, x ~ N(m, s)
+# where m = ((n / sigma_e^2) * y + (1 / sigma_mean^2) * mean_beta_i) / ((n / sigma_e^2) + (1 / sigma_mean^2))
+# and s = ((n / sigma_e^2) + (1 / sigma_mean^2))^(-1)
+
+# The marginal of y | x, Hi is no longer N(beta_i * x, sigma_e^2), since now we beta_i has a (prior) distribution
+# hence, using iterated expectation and variance,
+# y | x, Hi ~ N(mean_beta_i * x, sigma_e^2 + x^2 * sigma_mean^2)
 
 library(transport)
 
 ### Helper  Functions ###
 
 # Wasserstein distance betwen two (univariate) normals, N(mu1, var1) and N(mu2, var2)
-Wasserstein_distance = function(x, beta0, beta1, var_e){
-  mu1 = x * beta0
-  mu2 = x * beta1
-  var1 = var_e
-  var2 = var_e
-  return (mu1 - mu2)^2 + var1 + var2 - 2 * sqrt(var1 * var2)
+Wasserstein_distance = function(mu1, mu2, var_1, var_2){
+  return(mu1 - mu2)^2 + var1 + var2 - 2 * sqrt(var1 * var2)
 }
 
 # charge function at design point x
-q = function(x, beta0, beta1, var_e) 1.0 / Wasserstein_distance(x, beta0, beta1, var_e)
+q = function(x, beta0, beta1, var_e, var_mean){
+  mu1 = beta0 * x # mean of marginal dist of y | H0
+  mu2 = beta1 * x # mean of marginal dist of y | H1
+  var = var_e + x^2 * var_mean # variance of marginal dist of y | H1
+  Wass_dist = Wasserstein_distance(mu1, mu2, var, var)
+  return(1.0 / Wass_dist)
+}
 
 # minimizing criterion for greedy algorithm - calculate this for each x_i in candidate set,
 # select the one with the smallest value of f_min to add to current design set D
-f_min <- Vectorize(function(candidate, D, k, beta0, beta1, var_e) {
+f_min <- Vectorize(function(candidate, D, k, mean_beta0, mean_beta1, var_e, var_mean) {
   # xnew = an x from candidate set, xall = D, design points
-  q(candidate, beta0, beta1, var_e)^k * 
-    sum(sapply(D, function(x) (q(x, beta0, beta1, var_e) / abs(x - candidate))^k))
+  q(candidate, mean_beta0, mean_beta0, var_e, var_mean)^k * 
+    sum(sapply(D, function(x) (q(x, mean_beta0, mean_beta1, var_e, var_mean) / abs(x - candidate))^k))
 }, vectorize.args='candidate')
 
 ### Iterative Algorithm for SMED for Model Selection ###
@@ -40,11 +56,11 @@ f_min <- Vectorize(function(candidate, D, k, beta0, beta1, var_e) {
 
 # f is normal, so need to specify f0 = {beta0}, f1 = {beta1}, var_e
 # since these determine both f0 and f1's normal parameters 
-SMED_ms = function(f0, beta0, f1, beta1, var_e, n = 10, numCandidates = 1000, k = 4, xmin = 0, xmax = 1, 
-                   randomCandidates = T, initialization = 0
-                   ){
-  #  f0, beta0 : regression line and slope of null model
-  #  f1, beta1 : regression line and slope of alternative model
+SMED_ms = function(mean_beta0, mean_beta1, var_e, var_mean, n = 10, numCandidates = 1000, 
+                   k = 4, xmin = 0, xmax = 1, 
+                   randomCandidates = T, initialization = 0){
+  #  f0, mean_beta0 : regression line and mean slope of null model
+  #  f1, mean_beta1 : regression line and mean slope of alternative model
   #  n : number of design points to select (for set of design points, D)
   #  numCandidates : # of points to use as candidates (set from which design points are selected)
   #  k : power to use for MED. k = 4p is default
@@ -54,6 +70,21 @@ SMED_ms = function(f0, beta0, f1, beta1, var_e, n = 10, numCandidates = 1000, k 
   # initialization : method of initializing first design point of D
   #                  0 = candidate that outputs the largest absolute difference between f0 and f1
   #                  1 = candidate that has the largest ratio of likelihoods
+  
+  # Draw a slope for each model
+  beta0 = rnorm(n = 1, mean = mean_beta0, sd = sqrt(var_mean))
+  beta1 = rnorm(n = 1, mean = mean_beta1, sd = sqrt(var_mean))
+  
+  # Create linear model
+  f0 = function(x) beta0 * x # null regression model
+  f1 = function(x) beta1 * x # alternative regression model
+  
+  # Calculate posterior means and variance for the mean beta_i given the data y - DON'T NEED THESE.
+  # where m = ((1 / sigma_e^2) * y + (1 / var_mean^2) * mean_beta_i) / ((1 / sigma_e^2) + (1 / var_mean^2))
+  # and s = ((1 / sigma_e^2) + (1 / var_mean^2))^(-1)
+  # post_beta0 = ((1 / sigma_e^2) * y + (1 / var_mean^2) * mean_beta0) / ((1 / sigma_e^2) + (1 / var_mean^2))
+  # post_beta1 = ((1 / sigma_e^2) * y + (1 / var_mean^2) * mean_beta1) / ((1 / sigma_e^2) + (1 / var_mean^2))
+  # post_var = ((1 / sigma_e^2) + (1 / var_mean^2))^(-1)
   
   # -- Generate Candidate Points -- #
   if(randomCandidates == T){
@@ -71,7 +102,7 @@ SMED_ms = function(f0, beta0, f1, beta1, var_e, n = 10, numCandidates = 1000, k 
   } else if(initialization == 1){
     # initialization == 1: get the point at which f1 and f2 are most different
     xinitind = which.max(dnorm(x = candidates, mean = x * beta0, sd = sqrt(var_e)) / 
-                                 dnorm(x = candidates, mean = x * beta1, sd = sqrt(var_e)))
+                           dnorm(x = candidates, mean = x * beta1, sd = sqrt(var_e)))
   } else{
     warning("the initialization argument is not an option; default initialization is used.")
     xmostdifferentind = which.max(abs(f0(candidates) - f1(candidates)))
@@ -91,7 +122,8 @@ SMED_ms = function(f0, beta0, f1, beta1, var_e, n = 10, numCandidates = 1000, k 
   # Sequentially pick rest
   for(i in 2:n){
     # Find f_opt: minimum of f_min
-    f_opt = which.min(f_min(candidates, D, k, beta0, beta1, var_e))
+    
+    f_opt = which.min(f_min(candidates, D, k, mean_beta0, mean_beta1, var_e, var_mean))
     xnew = candidates[f_opt]
     # Update set of design points (D) and plot new point
     D = c(D,xnew) # add the new point to the set
@@ -106,12 +138,10 @@ SMED_ms = function(f0, beta0, f1, beta1, var_e, n = 10, numCandidates = 1000, k 
 # criterion (1 / d_W(f0, f1; x_i) * 1 / d_W(f0, f1; x_j)) / d(x_i, x_j)
 # here, we assume f's are normal, so we specify the mean and variance of each
 
-beta0 = 1 # slope of null model
-beta1 = 1 / 2 # slope of alternative model
+mean_beta0 = 1 # slope of null model
+mean_beta1 = 1 / 2 # slope of alternative model
+var_mean = 0.01
 var_e = 1 # same variance
-
-f0 = function(x) beta0 * x # null regression model
-f1 = function(x) beta1 * x # alternative regression model
 
 n = 10
 numCandidates = 1000
@@ -119,7 +149,7 @@ k = 4
 xmin = 0
 xmax = 1
 
-X_test = SMED_ms(f0, beta0, f1, beta1, var_e, n, numCandidates, k, xmin, xmax)
+X_test = SMED_ms(mean_beta0, mean_beta1, var_e, var_mean, n, numCandidates, k, xmin, xmax)
 
 # testing wasserstein function from "transport" library
 x = 0.3

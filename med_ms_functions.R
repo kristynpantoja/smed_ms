96320c948eee4533f0ff23e194cb8fa859295da4
Kristyn Pantoja <kristyn@Sandras-MacBook-Air.local>
2019-08-30 06:54:21 -0500

    changed posterior variance funcion

--- med_ms_functions.R
+++ med_ms_functions.R
@@ -262,9 +262,24 @@ MED_ms_fast = function(mean_beta0, mean_beta1, var_mean0, var_mean1, var_e,
 
 ### --- Posterior Variance --- ###
 
-postvar = function(X, var_e, var_mean){
-  if(is.null(dim(X)) | (dim(X)[2] == 1)) return(var_e * solve(crossprod(X) + var_e * (1 / var_mean))) # if X has one dimension
-  else return(var_e * solve(crossprod(X) + var_e * diag(1/diag(var_mean)))) # if X has multiple dimensions (i.e. beta is vector, not scalar)
+postvar = function(D, N, var_e, var_mean, type, diagPrior = TRUE){
+  X = NULL
+  if(type == 1) X = D
+  if(type == 2) X = cbind(rep(1, N), D)
+  if(type == 3) X = cbind(rep(1, N), D, D^2)
+  if(type == 4){
+    X = cbind(rep(1, N), D)
+  }
+  if(type == 5){
+    X = cbind(rep(1, N), D[,1], D[,1]^2, D[,2], D[,2]^2)
+  }
+  if(is.null(dim(X)) | (dim(X)[2] == 1)){ # if X has one dimension
+    if(diagPrior == TRUE) return(var_e * solve(crossprod(X) + var_e * (1 / var_mean)))
+    else return(var_e * solve(crossprod(X) + var_e * solve(var_mean)))
+  } else{ # if X has multiple dimensions (i.e. beta is vector, not scalar)
+    if(diagPrior == TRUE) return(var_e * solve(crossprod(X) + var_e * diag(1/diag(var_mean))))
+    else return(var_e * solve(crossprod(X) + var_e * solve(var_mean)))
+  }
 }
 
 # scaled prediction variance

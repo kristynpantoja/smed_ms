523f7aa4e31efa70329c4a21825d50fb5370ae6b
Kristyn Pantoja <kristynp@tamu.edu>
2019-09-16 13:57:22 -0500

    fixing mse calculation

--- functions/postmean_mse_mc.R
+++ functions/postmean_mse_mc.R
@@ -11,7 +11,7 @@ getPostMean = function(y, D, N, beta_prior_mean, beta_prior_var, var_e,
 }
 
 getEmpMSE = function(postmean_beta, true_beta){
-  return((postmean_beta - true_beta)^2)
+  return(sum((postmean_beta - true_beta)^2))
 }
 
 calcExpPostMeanMSE = function(D, N, true_beta, beta_prior_mean0, beta_prior_mean1, 
@@ -31,12 +31,12 @@ calcExpPostMeanMSE = function(D, N, true_beta, beta_prior_mean0, beta_prior_mean
     post_meansH0 = apply(Ysims, 2, FUN = function(y) getPostMean(y, D, N, beta_prior_mean0, beta_prior_var0, 
                                                                  var_e, type[1], diagPrior))
     empMSEsH0 = apply(post_meansH0, 2, FUN = function(x) getEmpMSE(x, true_beta[p0_indices_wrt_H1])) # Only MSE if assume H0 for estimation
-    expEmpiricalMSEH0 = apply(empMSEsH0, 1, mean)
+    expEmpiricalMSEH0 = mean(empMSEsH0)
     # posterior mean, given H1 prior on beta
     post_meansH1 = apply(Ysims, 2, FUN = function(y) getPostMean(y, D, N, beta_prior_mean1, beta_prior_var1, 
                                                                  var_e, type[2], diagPrior))
     empMSEsH1 = apply(post_meansH1, 2, FUN = function(x) getEmpMSE(x, true_beta))
-    expEmpiricalMSEH1 = apply(empMSEsH1, 1, mean)
+    expEmpiricalMSEH1 = mean(empMSEsH1)
   } else if(true_model_type == 4 & H01_model_types[1] == 4 & H01_model_types[2] == 5){
     # posterior mean, given H0 prior on beta
     p1_indices_wrt_H0 = c(1, 2, 4) # indices of parameters in alt model, i.e. intercept and linear terms
@@ -54,20 +54,20 @@ calcExpPostMeanMSE = function(D, N, true_beta, beta_prior_mean0, beta_prior_mean
     expEmpiricalMSEH0 = rep(NA, p0)
     expEmpiricalMSEH1 = rep(NA, p1)
   }
-  # put the results together
-  expEmpiricalMSEs = matrix(NA, p_true, 2)
-  if(true_model_type == 5 & H01_model_types[1] == 4 & H01_model_types[2] == 5){
-    expEmpiricalMSEs[p0_indices_wrt_H1, 1] = expEmpiricalMSEH0
-    expEmpiricalMSEs[1:p1, 2] = expEmpiricalMSEH1
-    colnames(expEmpiricalMSEs) = c("expEmpiricalMSEH0", "expEmpiricalMSEH1")
-    rownames(expEmpiricalMSEs) = paste("E[MSE(B", 0:(dim(expEmpiricalMSEs)[1] - 1), ")]", sep = "")
-  } else if(true_model_type == 4 & H01_model_types[1] == 4 & H01_model_types[2] == 5){
-    expEmpiricalMSEs[1:p0, 1] = expEmpiricalMSEH0
-    expEmpiricalMSEs[1:p0, 2] = expEmpiricalMSEH1
-    colnames(expEmpiricalMSEs) = c("expEmpiricalMSEH0", "expEmpiricalMSEH1")
-    rownames(expEmpiricalMSEs) = paste("E[MSE(B", 0:(dim(expEmpiricalMSEs)[1] - 1), ")]", sep = "")
-  }
-  return(expEmpiricalMSEs)
+  # # put the results together
+  # expEmpiricalMSEs = matrix(NA, p_true, 2)
+  # if(true_model_type == 5 & H01_model_types[1] == 4 & H01_model_types[2] == 5){
+  #   expEmpiricalMSEs[p0_indices_wrt_H1, 1] = expEmpiricalMSEH0
+  #   expEmpiricalMSEs[1:p1, 2] = expEmpiricalMSEH1
+  #   colnames(expEmpiricalMSEs) = c("expEmpiricalMSEH0", "expEmpiricalMSEH1")
+  #   rownames(expEmpiricalMSEs) = paste("E[MSE(B", 0:(dim(expEmpiricalMSEs)[1] - 1), ")]", sep = "")
+  # } else if(true_model_type == 4 & H01_model_types[1] == 4 & H01_model_types[2] == 5){
+  #   expEmpiricalMSEs[1:p0, 1] = expEmpiricalMSEH0
+  #   expEmpiricalMSEs[1:p0, 2] = expEmpiricalMSEH1
+  #   colnames(expEmpiricalMSEs) = c("expEmpiricalMSEH0", "expEmpiricalMSEH1")
+  #   rownames(expEmpiricalMSEs) = paste("E[MSE(B", 0:(dim(expEmpiricalMSEs)[1] - 1), ")]", sep = "")
+  # }
+  return(c("expEmpiricalMSEH0" = expEmpiricalMSEH0, "expEmpiricalMSEH1" = expEmpiricalMSEH1))
 }
 
 

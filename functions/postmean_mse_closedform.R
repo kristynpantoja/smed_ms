2eb1e0c17a3e80df1beb4277e1c1b105f4e43b78
Kristyn Pantoja <kristyn@main-10-230-22-6.tamulink.tamu.edu>
2019-10-08 12:25:50 -0500

    E[Bn] closed form

--- functions/postmean_mse_closedform.R
+++ functions/postmean_mse_closedform.R
@@ -1,9 +1,22 @@
 # require("construct_design_matrix.R")
 # require("posterior_variance.R")
 
-##########
-### 2D ###
-##########
+getClosedEBn = function(D, N, true_beta, beta_prior_mean, beta_prior_var, var_e, type, diagPrior = TRUE){
+  X = constructDesignX(D, N, type) # design matrix, depends on the model (type)
+  Sigma_B = postvar(D, N, var_e, beta_prior_var, type, diagPrior) # posterior variance
+  # 1. calculate the variance of the posterior mean
+  XtX = crossprod(X)
+  var_postmean_term1 = (1/var_e) * Sigma_B %*% XtX %*% Sigma_B
+  # var_postmean_term2 = (1/var_e)^2 * Sigma_B %*% XtX %*% beta_prior_var %*% XtX %*% Sigma_B
+  var_postmean_term2 = 0
+  # grab the variance terms in the variance-covariance matrix
+  var_postmean = diag(var_postmean_term1 + var_postmean_term2)
+  # 2. calculate expectation of posterior mean
+  # expect_postmean = (1/var_e) * Sigma_B %*% (XtX + var_e * solve(beta_prior_var)) %*% matrix(beta_prior_mean)
+  expect_postmean = (1/var_e) * Sigma_B %*% XtX %*% matrix(true_beta) + Sigma_B %*% solve(beta_prior_var) %*% matrix(beta_prior_mean)
+  return(list("EBn" = expect_postmean, "VarBn" = var_postmean))
+}
+
 
 getClosedMSE = function(D, N, true_beta, beta_prior_mean, beta_prior_var, var_e, type, diagPrior = TRUE){
   X = constructDesignX(D, N, type) # design matrix, depends on the model (type)

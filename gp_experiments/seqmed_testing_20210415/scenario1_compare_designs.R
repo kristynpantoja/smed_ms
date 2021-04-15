38aada07130f450ec57a533cd208aff47d11ca43
Kristyn Pantoja <kristynp@tamu.edu>
2021-04-15 09:34:29 -0500

    compare designs

---
+++ gp_experiments/seqmed_testing_20210415/scenario1_compare_designs.R
@@ -0,0 +1,284 @@
+################################################################################
+# last updated: 04/14/2021
+# purpose: to test seqmedgp for scenario 1:
+#   squared exponential vs. matern,
+#   where the true function is matern
+# trying out some (not necessarily MED) designs
+# changed SeqMEDgp to take in model0, model1
+
+################################################################################
+# Sources/Libraries
+################################################################################
+output_home = "gp_experiments/seqmed_testing_20210415/outputs"
+functions_home = "functions"
+
+# for seqmed design
+source(paste(functions_home, "/SeqMEDgp.R", sep = ""))
+source(paste(functions_home, "/SeqMEDgp_batch.R", sep = ""))
+source(paste(functions_home, "/charge_function_q.R", sep = ""))
+source(paste(functions_home, "/covariance_functions.R", sep = ""))
+source(paste(functions_home, "/wasserstein_distance.R", sep = ""))
+source(paste(functions_home, "/gp_predictive.R", sep = ""))
+
+# for box-hill design
+source(paste(functions_home, "/boxhill.R", sep = ""))
+source(paste(functions_home, "/boxhill_gp.R", sep = ""))
+source(paste(functions_home, "/kl_divergence.R", sep = ""))
+
+library(mvtnorm)
+
+# set up parallelization
+library(foreach)
+library(future)
+library(doFuture)
+library(parallel)
+registerDoFuture()
+nworkers = detectCores()
+plan(multisession, workers = nworkers)
+
+library(rngtools)
+library(doRNG)
+rng.seed = 123 # 123, 345
+registerDoRNG(rng.seed)
+
+library(ggplot2)
+library(reshape2)
+gg_color_hue = function(n) {
+  hues = seq(15, 275, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
+################################################################################
+# simulation settings, shared for both scenarios
+################################################################################
+# errorvar.type = 1 # 1 = phi0 with nugget, 2 = phi1 with nugget
+# signalvar.type = 2 # 1 = phi0 sigmasq != 1, 2 = phi1 sigmasq != 1
+input.type = 1 # 1 = extrapolation, 2 = inc spread, 3 = even coverage
+seq.type = 1 # 1 = fully sequential, 2 = stage-sequential 3x5
+
+# simulations settings
+numSims = 25
+Nin = 6
+if(seq.type == 1){
+  numSeq = 15
+  seqN = 1
+} else if(seq.type == 2){
+  numSeq = 3
+  seqN = 5
+}
+Nnew = numSeq * seqN
+Nttl = Nin + Nnew
+xmin = 0
+xmax = 1
+numx = 10^3 + 1
+x_seq = seq(from = xmin, to = xmax, length.out = numx)
+
+# SeqMED settings
+sigmasqs = c(1 - 1e-10, 1)
+nuggets = c(1e-10, 1e-15)
+nugget.sm = NULL
+buffer = 0
+
+# boxhill settings
+nugget.bh = 1e-10
+prior_probs = rep(1 / 2, 2)
+
+# shared settings
+sigmasq = 1
+
+################################################################################
+# input data
+################################################################################
+
+# 1. make space-filling design
+# space_filling = seq(from = xmin, to = xmax, length.out = Nttl)
+space_filling_idx = c(1, 1 + ((numx - 1)/(Nttl - 1)) * 1:((numx - 1) / ((numx - 1)/(Nttl - 1))))
+space_filling = x_seq[space_filling_idx]
+
+# input set 1 (extrapolation)
+x_in1_idx = space_filling_idx[1:Nin]
+x_in1 = x_seq[x_in1_idx]
+x_spacefill1_idx = space_filling_idx[-c(1:Nin)]
+x_spacefill1 = x_seq[x_spacefill1_idx]
+# all.equal(space_filling, c(x_in1, x_spacefill1))
+
+# input set 2 (increasing spread)
+x_in2_idx = space_filling_idx[c(1, 2, 4, 7, 12, 21)]
+x_in2 = x_seq[x_in2_idx]
+x_spacefill2_idx = space_filling_idx[-c(1, 2, 4, 7, 12, 21)]
+x_spacefill2 = x_seq[x_spacefill2_idx]
+# all.equal(space_filling, sort(c(x_in2, x_spacefill2)))
+
+# input set 3 (space-filling / even coverage)
+x_in3_idx = c(1, 1 + ((numx - 1)/(Nin - 1)) * 1:((numx - 1) / ((numx - 1)/(Nin - 1))))
+x_in3 = x_seq[x_in3_idx]
+x_spacefill3_idx = space_filling_idx[!(space_filling_idx %in% x_in3_idx)]
+x_spacefill3 = x_seq[x_spacefill3_idx]
+# all.equal(space_filling, sort(c(x_in3, x_spacefill3)))
+
+# input set 4 (uniform / random)
+
+################################################################################
+# Scenario 1: Squared exponential vs. matern, true = matern
+################################################################################
+type01 = c("squaredexponential", "matern")
+l01= c(0.01, 0.01) # SIM SETTING
+# l01= c(0.1, 0.1) # DEMO SETTING
+
+################################################################################
+# models - BoxHill & q
+model0.bhq = list(type = type01[1], l = l01[1], signal.var = sigmasq, 
+                  error.var = nugget.bh)
+model1.bhq = list(type = type01[2], l = l01[2], signal.var = sigmasq, 
+                  error.var = nugget.bh)
+
+# models - SeqMED with different nugget term
+# errorvar.type == 1
+model0.n1 = list(type = type01[1], l = l01[1], signal.var = sigmasq, 
+                 error.var = nuggets[1])
+model1.n1 = list(type = type01[2], l = l01[2], signal.var = sigmasq, 
+                 error.var = nuggets[2])
+# errorvar.type == 2
+model0.n2 = list(type = type01[1], l = l01[1], signal.var = sigmasq,
+                 error.var = nuggets[2])
+model1.n2 = list(type = type01[2], l = l01[2], signal.var = sigmasq, 
+                 error.var = nuggets[1])
+
+# models - SeqMED with different signal variance
+# signalvar.type == 1
+model0.s1 = list(type = type01[1], l = l01[1], signal.var = sigmasqs[1], 
+                 error.var = nugget.sm)
+model1.s1 = list(type = type01[2], l = l01[2], signal.var = sigmasqs[2], 
+                 error.var = nugget.sm)
+# signalvar.type == 2
+model0.s2 = list(type = type01[1], l = l01[1], signal.var = sigmasqs[2],
+                 error.var = nugget.sm)
+model1.s2 = list(type = type01[2], l = l01[2], signal.var = sigmasqs[1], 
+                 error.var = nugget.sm)
+
+################################################################################
+# import matern functions
+simulated.functions = readRDS(paste0(
+  output_home,
+  "/scenario1_simulated_functions", 
+  "_seed", rng.seed,
+  ".rds"))
+numSims = simulated.functions$numSims
+x_seq = simulated.functions$x
+numx = length(x_seq)
+null_cov = simulated.functions$null_cov
+null_mean = simulated.functions$null_mean
+y_seq_mat = simulated.functions$function_values_mat
+
+################################################################################
+# read in the data
+
+boxhills = readRDS(paste0(
+  output_home, 
+  "/scenario1_boxhill", 
+  "_input", input.type, 
+  "_seed", rng.seed, 
+  ".rds"
+))
+
+qs = readRDS(paste0(
+  output_home,
+  "/scenario1_seqmed",
+  "_obj", 2,
+  "_input", input.type,
+  "_seq", seq.type,
+  "_seed", rng.seed,
+  ".rds"
+))
+
+seqmeds.n1 = readRDS(paste0(
+  output_home,
+  "/scenario1_seqmed", 
+  "_obj", 1,
+  "_error", 1, 
+  "_input", input.type, 
+  "_seq", seq.type,
+  "_seed", rng.seed,
+  ".rds"
+))
+
+seqmeds.n2 = readRDS(paste0(
+  output_home,
+  "/scenario1_seqmed", 
+  "_obj", 1,
+  "_error", 2, 
+  "_input", input.type, 
+  "_seq", seq.type,
+  "_seed", rng.seed,
+  ".rds"
+))
+
+seqmeds.s1 = readRDS(paste0(
+  output_home,
+  "/scenario1_seqmed", 
+  "_obj", 1,
+  "_signal", 1, 
+  "_input", input.type, 
+  "_seq", seq.type,
+  "_seed", rng.seed,
+  ".rds"
+))
+
+seqmeds.s2 = readRDS(paste0(
+  output_home,
+  "/scenario1_seqmed", 
+  "_obj", 1,
+  "_signal", 2, 
+  "_input", input.type, 
+  "_seq", seq.type,
+  "_seed", rng.seed,
+  ".rds"
+))
+
+################################################################################
+# make plots
+################################################################################
+
+# input set
+if(input.type == 1){
+  x_input = x_in1
+  x_input_idx = x_in1_idx
+} else if(input.type == 2){
+  x_input = x_in2
+  x_input_idx = x_in2_idx
+} else if(input.type == 3){
+  x_input = x_in3
+  x_input_idx = x_in3_idx
+}
+
+# all 6 designs
+idx = 1
+designs = list(boxhills[[idx]], qs[[idx]], seqmeds.n1[[idx]], seqmeds.n2[[idx]], 
+               seqmeds.s1[[idx]], seqmeds.s2[[idx]])
+design.names = c("bh", "q", "nugget1", "nugget2", "signal1", "signal2")
+
+x.new.mat = matrix(NA, nrow = Nnew, ncol = length(designs))
+for(i in 1:length(designs)){
+  x.new.mat[, i] = designs[[i]]$x.new
+}
+
+data.gg = data.frame(
+  index = as.character(rep(1:Nnew, length(designs))), 
+  type = factor(rep(design.names, each = Nnew), levels = design.names), 
+  value = as.vector(x.new.mat)
+)
+data.gg0 = data.frame(
+  type = factor(rep(design.names, each = Nin), levels = design.names), 
+  input = rep(x_input, length(designs))
+)
+text.gg = dplyr::filter(data.gg, index %in% as.character(1:10))
+ggplot() + 
+  geom_point(data = data.gg0, 
+             mapping = aes(x = input, y = type)) +
+  geom_point(data = data.gg, 
+             mapping = aes(x = value, y = type, color = type), 
+             inherit.aes = FALSE) + 
+  geom_text(data = text.gg, 
+            aes(x = value, y = type, label = index), 
+            vjust = -0.75 * as.numeric(paste(text.gg$index)), size = 2) +
+  xlim(c(xmin, xmax))

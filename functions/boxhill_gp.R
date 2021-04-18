db6c4b40004e9c17a9a83e899f27f8f100abdf4f
Kristyn Pantoja <kristynp@tamu.edu>
2021-04-17 19:06:46 -0500

    fixed box and hill checking post prob = 0, 1

--- functions/boxhill_gp.R
+++ functions/boxhill_gp.R
@@ -110,8 +110,8 @@ BHgp_m2 = function(
       )
     )
     post.probs.mat[i + 1, ] = post.probs.cur
-    # check post.probs.cur
-    if(all(post.probs.cur %in% c(0, 1))) break
+    # check post.probs.cur -- if either equals 0 or 1
+    if(sum(post.probs.cur %in% c(0, 1)) > 0) break
   }
   return(list(
     x = x, 

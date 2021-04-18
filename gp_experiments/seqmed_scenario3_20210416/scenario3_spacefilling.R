809240b9907d699c17847eee801aa54f3ba46253
Kristyn Pantoja <kristynp@tamu.edu>
2021-04-17 22:22:26 -0500

    new l0

--- gp_experiments/seqmed_scenario3_20210416/scenario3_spacefilling.R
+++ gp_experiments/seqmed_scenario3_20210416/scenario3_spacefilling.R
@@ -49,14 +49,11 @@ gg_color_hue = function(n) {
 ################################################################################
 # simulation settings, shared for both scenarios
 ################################################################################
-input.type = 1 # 1 = extrapolation, 2 = inc spread, 3 = even coverage
 
 # simulations settings
 numSims = 25
 Nin = 6
-numSeq = 15
-seqN = 1
-Nnew = numSeq * seqN
+Nnew = 15
 Nttl = Nin + Nnew
 xmin = 0
 xmax = 1
@@ -104,7 +101,7 @@ x_spacefill3 = x_seq[x_spacefill3_idx]
 ################################################################################
 type01 = c("squaredexponential", "squaredexponential")
 typeT = "matern"
-l01= c(0.02, 0.01)
+l01= c(0.005, 0.01)
 lT = 0.01
 
 ################################################################################
@@ -131,48 +128,50 @@ y_seq_mat = simulated.functions$function_values_mat
 ################################################################################
 # space-filling design
 
-# input set
-if(input.type == 1){
-  x_input = x_in1
-  x_input_idx = x_in1_idx
-} else if(input.type == 2){
-  x_input = x_in2
-  x_input_idx = x_in2_idx
-} else if(input.type == 3){
-  x_input = x_in3
-  x_input_idx = x_in3_idx
-}
-
-# space-filling
-registerDoRNG(rng.seed)
-randoms = foreach(
-  i = 1:numSims
-) %dorng% {
-  y_seq = y_seq_mat[ , i]
-  y_input = y_seq[x_input_idx]
+for(j in 1:3){
   
-  # new points
+  # j : input setting
+  input.type = j
+  # input set
   if(input.type == 1){
+    x_input = x_in1
+    x_input_idx = x_in1_idx
+    # new points
     x.new = x_spacefill1
     x.new.idx = x_spacefill1_idx
   } else if(input.type == 2){
+    x_input = x_in2
+    x_input_idx = x_in2_idx
+    # new points
     x.new = x_spacefill2
     x.new.idx = x_spacefill2_idx
   } else if(input.type == 3){
+    x_input = x_in3
+    x_input_idx = x_in3_idx
+    # new points
     x.new = x_spacefill3
     x.new.idx = x_spacefill3_idx
   }
   
-  y.new = y_seq[x.new.idx]
-  list(x = x_input, x.idx = x_input_idx, y = y_input, 
-       x.new = x.new, x.new.idx = x.new.idx, y.new = y.new, 
-       function.values = y_seq)
+  # simulations!
+  registerDoRNG(rng.seed)
+  randoms = foreach(
+    i = 1:numSims
+  ) %dorng% {
+    y_seq = y_seq_mat[ , i]
+    y_input = y_seq[x_input_idx]
+    # new points' y
+    y.new = y_seq[x.new.idx]
+    list(x = x_input, x.idx = x_input_idx, y = y_input, 
+         x.new = x.new, x.new.idx = x.new.idx, y.new = y.new, 
+         function.values = y_seq)
+  }
+  
+  saveRDS(randoms, 
+          file = paste0(
+            output_home,
+            "/scenario3_spacefilling", 
+            "_input", input.type, 
+            "_seed", rng.seed,
+            ".rds"))
 }
-
-saveRDS(randoms, 
-        file = paste0(
-          output_home,
-          "/scenario3_spacefilling", 
-          "_input", input.type, 
-          "_seed", rng.seed,
-          ".rds"))

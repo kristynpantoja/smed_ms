34e47bf1205cce0e39417e2e41bb375c92d5b6d3
kristynpantoja <kristynp@tamu.edu>
2021-09-12 16:54:48 -0500

    simY as.vector()

--- functions/postprob_hypotheses.R
+++ functions/postprob_hypotheses.R
@@ -1,149 +1,149 @@
-# require("construct_design_matrix.R")
-# require("simulate_y.R")
-
-model_evidence = function(Y, D, N, beta_prior_mean, beta_prior_var, var_e, hypothesis_model_type, indices = NULL){
-  # Y is a vector of outputs (or scalar, for one output)
-  # X is a matrix of inputs (or vector, for one input)
-  # beta_prior_var is a matrix
-  # var_e is a scalar
-  if(N != length(Y)) stop("N is not the same length as Y")
-  if(!is.null(hypothesis_model_type)){
-    X = constructDesignX(D, N, hypothesis_model_type)
-  } else{
-    X = constructDesignX(D, N, hypothesis_model_type)[ , indices]
-  }
-  # get mean and variance of marginal density of y, which is N-dim multivariate normal pdf
-  marginaly_mean = X %*% beta_prior_mean
-  if(dim(X)[1] > 1){ # if X is a matrix of inputs
-    marginaly_var = diag(rep(var_e, N)) + (X %*% beta_prior_var %*% t(X))
-  } else{ # if X is a vector for one input
-    warning("X is a vector, not a matrix - is that what you expected?")
-    marginaly_var = var_e + (X %*% beta_prior_var %*% t(X))
-  }
-  return(dmvnorm(Y, mean = marginaly_mean, sigma = marginaly_var, log = FALSE))
-}
-
-######################
-## for 2 hypotheses ##
-######################
-
-calcExpPostProbH = function(
-  D, N, true_beta, beta_prior_mean0, beta_prior_mean1, 
-  beta_prior_var0, beta_prior_var1, var_e,
-  numSims = 100, true_model_type = NULL, H01_model_types = NULL,
-  seed = NULL, saveSims = FALSE
-){
-  # for now we assume that the type of model for true_beta contains the types for both hypotheses
-  simY = simulateY(D, N, true_beta, var_e, numSims, true_model_type, seed)
-  simPostH0 = rep(NA, numSims)
-  simPostH1 = rep(NA, numSims)
-  simBF01 = rep(NA, numSims)
-  for(j in 1:numSims){
-    Y = simY[ , j]
-    # get model evidences for each hypothesized model
-    simEvidenceH0 = model_evidence(Y, D, N, beta_prior_mean0, beta_prior_var0, var_e, H01_model_types[1])
-    simEvidenceH1 = model_evidence(Y, D, N, beta_prior_mean1, beta_prior_var1, var_e, H01_model_types[2])
-    # calculate posterior probabilities of each hypothesis
-    simPostH0[j] = simEvidenceH0 / (simEvidenceH0 + simEvidenceH1)
-    simPostH1[j] = simEvidenceH1 / (simEvidenceH0 + simEvidenceH1)
-    # calculate bayes factor
-    simBF01[j] = simEvidenceH0 / simEvidenceH1
-  }
-  expected_postH0 = mean(simPostH0)
-  expected_postH1 = mean(simPostH1)
-  expected_BF01 = mean(simBF01)
-  
-  if(saveSims){
-    return(list(
-      "H0" = expected_postH0, 
-      "H1" = expected_postH1,
-      "BF01" = expected_BF01, 
-      "H0sims" = simPostH0, 
-      "H1sims" = simPostH1))
-  } else{
-    return(c(
-      "H0" = expected_postH0, 
-      "H1" = expected_postH1,
-      "BF01" = expected_BF01
-    ))
-  }
-}
-
-calcExpPostProbH_data = function(
-  y, D, N, beta_prior_mean0, beta_prior_var0, 
-  beta_prior_mean1, beta_prior_var1, var_e, model_types
-){
-  # get model evidence
-  evidence0 = model_evidence(y, D, N, beta_prior_mean0, beta_prior_var0, var_e, model_types[1])
-  evidence1 = model_evidence(y, D, N, beta_prior_mean1, beta_prior_var1, var_e, model_types[2])
-  # get each hypotheses' posterior probability
-  # calculate posterior probabilities of each hypothesis
-  postprob0 = evidence0 / (evidence0 + evidence1)
-  postprob1 = evidence1 / (evidence0 + evidence1)
-  BF01 = evidence0 / evidence1
-  
-  return(c("postprob0" = postprob0, "postprob1" = postprob1, "BF01" = BF01))
-}
-
-
-######################
-## for M hypotheses ##
-######################
-
-# for non-sequential designs, where data needs to be generated in simulations to estimate
-calcEPPH = function(
-  D, N, true_beta, true_model_type, models, var_e, numSims = 100, 
-  true_indices = NULL, seed = NULL, saveSims = FALSE
-  ){
-  if(!is.null(true_model_type)){
-    simY = simulateY(D, N, true_beta, var_e, numSims, true_model_type, seed)
-  } else{
-    simY = simulateYvs(D[ , true_indices], N, true_beta, var_e, numSims, seed)
-  }
-  # "models" is a list of lists, where each element of list "models" describes a model
-  #   each element/model is a list containing "beta_prior_mean", "beta_prior_var", "model_type"
-  #   in that order
-  #   if the model type is for variable selection, i.e. model_type == NULL, there will be a fourth element
-  #   in the model's list: indices, which corresponds to its hypothesized beta_prior_mean
-  # for now we assume that the type of model for true_beta contains the types for both hypotheses
-  # model_evidences = matrix(NA, length(models), numSims)
-  model_postprobs = matrix(NA, length(models), numSims)
-  for(j in 1:numSims){
-    Y = simY[ , j]
-    model_postprobs[ , j] = calcEPPHdata(Y, D, N, models, var_e)
-  }
-  exp_postprobs = apply(model_postprobs, 1, mean)
-  
-  if(saveSims){
-    return(list("exp_postprobs" = exp_postprobs, "sims" = model_postprobs))
-  } else{
-    return(c("exp_postprobs" = exp_postprobs))
-  }
-}
-
-# for designs with data
-calcEPPHdata = function(y, D, N, models, var_e){
-  model_evidences = rep(NA, length(models))
-  model_postprobs = rep(NA, length(models))
-  # get model evidence
-  for(m in 1:length(models)){
-    model = models[[m]]
-    if(length(model) == 3) model[[4]] = NULL
-    model_evidences[m] = model_evidence(y, D, N, model[[1]], model[[2]], var_e, model[[3]], model[[4]])
-  }
-  # get each hypotheses' posterior probability
-  denom = sum(model_evidences)
-  model_postprobs = model_evidences / denom
-  return(c("model_postprobs" = model_postprobs))
-}
-
-calcEPPHseqdata = function(y, D, models, var_e, initN, numSeq, N_seq){
-  postprobs = matrix(NA, length(models), numSeq + 1)
-  for(i in 0:numSeq){
-    seq_ind = 1:(initN + N_seq * i)
-    changing_postprobs = calcEPPHdata(y[seq_ind], D[seq_ind, , drop = FALSE], 
-                                      N = initN + N_seq * i, models, var_e)
-    postprobs[ , i + 1] = changing_postprobs
-  }
-  return(postprobs)
-}
+# # require("construct_design_matrix.R")
+# # require("simulate_y.R")
+# 
+# model_evidence = function(Y, D, N, beta_prior_mean, beta_prior_var, var_e, hypothesis_model_type, indices = NULL){
+#   # Y is a vector of outputs (or scalar, for one output)
+#   # X is a matrix of inputs (or vector, for one input)
+#   # beta_prior_var is a matrix
+#   # var_e is a scalar
+#   if(N != length(Y)) stop("N is not the same length as Y")
+#   if(!is.null(hypothesis_model_type)){
+#     X = constructDesignX(D, N, hypothesis_model_type)
+#   } else{
+#     X = constructDesignX(D, N, hypothesis_model_type)[ , indices]
+#   }
+#   # get mean and variance of marginal density of y, which is N-dim multivariate normal pdf
+#   marginaly_mean = X %*% beta_prior_mean
+#   if(dim(X)[1] > 1){ # if X is a matrix of inputs
+#     marginaly_var = diag(rep(var_e, N)) + (X %*% beta_prior_var %*% t(X))
+#   } else{ # if X is a vector for one input
+#     warning("X is a vector, not a matrix - is that what you expected?")
+#     marginaly_var = var_e + (X %*% beta_prior_var %*% t(X))
+#   }
+#   return(dmvnorm(Y, mean = marginaly_mean, sigma = marginaly_var, log = FALSE))
+# }
+# 
+# ######################
+# ## for 2 hypotheses ##
+# ######################
+# 
+# calcExpPostProbH = function(
+#   D, N, true_beta, beta_prior_mean0, beta_prior_mean1, 
+#   beta_prior_var0, beta_prior_var1, var_e,
+#   numSims = 100, true_model_type = NULL, H01_model_types = NULL,
+#   seed = NULL, saveSims = FALSE
+# ){
+#   # for now we assume that the type of model for true_beta contains the types for both hypotheses
+#   simY = simulateY(D, N, true_beta, var_e, numSims, true_model_type, seed)
+#   simPostH0 = rep(NA, numSims)
+#   simPostH1 = rep(NA, numSims)
+#   simBF01 = rep(NA, numSims)
+#   for(j in 1:numSims){
+#     Y = simY[ , j]
+#     # get model evidences for each hypothesized model
+#     simEvidenceH0 = model_evidence(Y, D, N, beta_prior_mean0, beta_prior_var0, var_e, H01_model_types[1])
+#     simEvidenceH1 = model_evidence(Y, D, N, beta_prior_mean1, beta_prior_var1, var_e, H01_model_types[2])
+#     # calculate posterior probabilities of each hypothesis
+#     simPostH0[j] = simEvidenceH0 / (simEvidenceH0 + simEvidenceH1)
+#     simPostH1[j] = simEvidenceH1 / (simEvidenceH0 + simEvidenceH1)
+#     # calculate bayes factor
+#     simBF01[j] = simEvidenceH0 / simEvidenceH1
+#   }
+#   expected_postH0 = mean(simPostH0)
+#   expected_postH1 = mean(simPostH1)
+#   expected_BF01 = mean(simBF01)
+#   
+#   if(saveSims){
+#     return(list(
+#       "H0" = expected_postH0, 
+#       "H1" = expected_postH1,
+#       "BF01" = expected_BF01, 
+#       "H0sims" = simPostH0, 
+#       "H1sims" = simPostH1))
+#   } else{
+#     return(c(
+#       "H0" = expected_postH0, 
+#       "H1" = expected_postH1,
+#       "BF01" = expected_BF01
+#     ))
+#   }
+# }
+# 
+# calcExpPostProbH_data = function(
+#   y, D, N, beta_prior_mean0, beta_prior_var0, 
+#   beta_prior_mean1, beta_prior_var1, var_e, model_types
+# ){
+#   # get model evidence
+#   evidence0 = model_evidence(y, D, N, beta_prior_mean0, beta_prior_var0, var_e, model_types[1])
+#   evidence1 = model_evidence(y, D, N, beta_prior_mean1, beta_prior_var1, var_e, model_types[2])
+#   # get each hypotheses' posterior probability
+#   # calculate posterior probabilities of each hypothesis
+#   postprob0 = evidence0 / (evidence0 + evidence1)
+#   postprob1 = evidence1 / (evidence0 + evidence1)
+#   BF01 = evidence0 / evidence1
+#   
+#   return(c("postprob0" = postprob0, "postprob1" = postprob1, "BF01" = BF01))
+# }
+# 
+# 
+# ######################
+# ## for M hypotheses ##
+# ######################
+# 
+# # for non-sequential designs, where data needs to be generated in simulations to estimate
+# calcEPPH = function(
+#   D, N, true_beta, true_model_type, models, var_e, numSims = 100, 
+#   true_indices = NULL, seed = NULL, saveSims = FALSE
+#   ){
+#   if(!is.null(true_model_type)){
+#     simY = simulateY(D, N, true_beta, var_e, numSims, true_model_type, seed)
+#   } else{
+#     simY = simulateYvs(D[ , true_indices], N, true_beta, var_e, numSims, seed)
+#   }
+#   # "models" is a list of lists, where each element of list "models" describes a model
+#   #   each element/model is a list containing "beta_prior_mean", "beta_prior_var", "model_type"
+#   #   in that order
+#   #   if the model type is for variable selection, i.e. model_type == NULL, there will be a fourth element
+#   #   in the model's list: indices, which corresponds to its hypothesized beta_prior_mean
+#   # for now we assume that the type of model for true_beta contains the types for both hypotheses
+#   # model_evidences = matrix(NA, length(models), numSims)
+#   model_postprobs = matrix(NA, length(models), numSims)
+#   for(j in 1:numSims){
+#     Y = simY[ , j]
+#     model_postprobs[ , j] = calcEPPHdata(Y, D, N, models, var_e)
+#   }
+#   exp_postprobs = apply(model_postprobs, 1, mean)
+#   
+#   if(saveSims){
+#     return(list("exp_postprobs" = exp_postprobs, "sims" = model_postprobs))
+#   } else{
+#     return(c("exp_postprobs" = exp_postprobs))
+#   }
+# }
+# 
+# # for designs with data
+# calcEPPHdata = function(y, D, N, models, var_e){
+#   model_evidences = rep(NA, length(models))
+#   model_postprobs = rep(NA, length(models))
+#   # get model evidence
+#   for(m in 1:length(models)){
+#     model = models[[m]]
+#     if(length(model) == 3) model[[4]] = NULL
+#     model_evidences[m] = model_evidence(y, D, N, model[[1]], model[[2]], var_e, model[[3]], model[[4]])
+#   }
+#   # get each hypotheses' posterior probability
+#   denom = sum(model_evidences)
+#   model_postprobs = model_evidences / denom
+#   return(c("model_postprobs" = model_postprobs))
+# }
+# 
+# calcEPPHseqdata = function(y, D, models, var_e, initN, numSeq, N_seq){
+#   postprobs = matrix(NA, length(models), numSeq + 1)
+#   for(i in 0:numSeq){
+#     seq_ind = 1:(initN + N_seq * i)
+#     changing_postprobs = calcEPPHdata(y[seq_ind], D[seq_ind, , drop = FALSE], 
+#                                       N = initN + N_seq * i, models, var_e)
+#     postprobs[ , i + 1] = changing_postprobs
+#   }
+#   return(postprobs)
+# }
--- functions/simulate_y.R
+++ functions/simulate_y.R
@@ -1,41 +1,42 @@
-# require("construct_design_matrix.R")
-
-simulateY = function(D, N, true_beta, var_e, numSims, type = NULL, seed = NULL){
-  # note that type is based on what type of model for true_beta.
-  # if no type given, assume multiple linear regression
-  if(!is.null(seed)) set.seed(seed)
-  if(is.null(type)){ # multidimensional case
-    if(dim(D)[2] != length(true_beta)) stop("dimension of D doesn't match length of true_beta in case where type == NULL")
-    X = D
-  } else{ # regress on transformations of x
-    X = constructDesignX(D, N, type)
-  }
-  Y = matrix(rep(NA, N * numSims), N, numSims) # each column is a separate simulation
-  for(j in 1:numSims){
-    # Y[ , j] = rmvnorm(1, X %*% true_beta, var_e * diag(N))
-    Y[ , j] = X %*% true_beta + sqrt(var_e) * rnorm(N)
-  }
-  return(Y)
-}
-
-simulateYvs = function(D, N, true_beta, var_e, numSims, seed = NULL){
-  simulateY(D, N, true_beta, var_e, numSims, type = NULL, seed = NULL)
-}
-
-# simulateYN = function(X, true_beta, var_e, numSims, seed = NULL){
+# # require("construct_design_matrix.R")
+# 
+# simulateY = function(D, N, true_beta, var_e, numSims, type = NULL, seed = NULL){
+#   # note that type is based on what type of model for true_beta.
+#   # if no type given, assume multiple linear regression
 #   if(!is.null(seed)) set.seed(seed)
-#   if(dim(X)[2] != length(true_beta)) stop("simulateYN : X and true_beta aren't compatible!")
+#   if(is.null(type)){ # multidimensional case
+#     if(dim(D)[2] != length(true_beta)) stop("dimension of D doesn't match length of true_beta in case where type == NULL")
+#     X = D
+#   } else{ # regress on transformations of x
+#     X = constructDesignX(D, N, type)
+#   }
 #   Y = matrix(rep(NA, N * numSims), N, numSims) # each column is a separate simulation
-#   # N x numSims -- each row is a simulation of N data points, 
-#   # where N is the number of rows in design matrix X
-#   Y = rmvnorm(numSims, X %*% true_beta, var_e * diag(N))
+#   for(j in 1:numSims){
+#     # Y[ , j] = rmvnorm(1, X %*% true_beta, var_e * diag(N))
+#     Y[ , j] = X %*% true_beta + sqrt(var_e) * rnorm(N)
+#   }
+#   return(Y)
 # }
+# 
+# simulateYvs = function(D, N, true_beta, var_e, numSims, seed = NULL){
+#   simulateY(D, N, true_beta, var_e, numSims, type = NULL, seed = NULL)
+# }
+# 
+# # simulateYN = function(X, true_beta, var_e, numSims, seed = NULL){
+# #   if(!is.null(seed)) set.seed(seed)
+# #   if(dim(X)[2] != length(true_beta)) stop("simulateYN : X and true_beta aren't compatible!")
+# #   Y = matrix(rep(NA, N * numSims), N, numSims) # each column is a separate simulation
+# #   # N x numSims -- each row is a simulation of N data points, 
+# #   # where N is the number of rows in design matrix X
+# #   Y = rmvnorm(numSims, X %*% true_beta, var_e * diag(N))
+# # }
 
 simulateY_fromfunction = function(
   x, true.function, error.var, seed = NULL
 ){
   if(!is.null(seed)) set.seed(seed)
   Y = true.function(x) + rnorm(length(x), 0, sqrt(error.var))
+  return(as.vector(Y))
 }
 
 simulateY_frommultivarfunction = function(
@@ -45,4 +46,5 @@ simulateY_frommultivarfunction = function(
   # if x isn't in matrix form, turn it into a matrix
   if(!("matrix" %in% class(x))) x = t(matrix(x))
   Y = true.function(x) + rnorm(nrow(x), 0, sqrt(error.var))
+  return(as.vector(Y))
 }
